758b5d941f889fada6bbb31998e578c00ae858ce
Fethiya Seid <110985471+fethiya37@users.noreply.github.com>
2025-09-30 14:17:33 +0300

    type eror

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './presentation/auth/auth.module';
 import { UserModule } from './presentation/user/user.module';
 import { AssociationModule } from './presentation/association/association.module';
@@ -36,8 +34,7 @@ import { SmsModule } from './presentation/sms/sms.module';
     JobsModule,
     SmsModule
   ],
-  controllers: [AppController],
-  providers: [AppService,
+  providers: [
     { provide: APP_GUARD, useClass: JwtAuthGuard }, // every route needs a valid JWT
     { provide: APP_GUARD, useClass: RolesGuard },   // @Roles() works anywhere
   ],
